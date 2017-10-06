5d76e93bfecdc932c8a685ff82f41541f90338c4
mmadrigal <mmadrigal@cambridgeassociates.com>
2017-10-05 22:08:29 -0600

    basic project structure

---
+++ asset-manager-app/asset-manager-webapp/src/main/java/com/asset/manager/entity/Liquidity.java
@@ -0,0 +1,28 @@
+package com.asset.manager.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "LIQUIDITY")
+@EqualsAndHashCode(callSuper=false)
+@ToString
+public class Liquidity extends BaseEntity
+{
+	@Setter
+	@Getter
+	@Id
+	@Column(name = "LIQUIDITY_PK", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private BigInteger id;
+
+	@Setter
+	@Getter
+	@Column(name = "Liquidity_Name", length = 250)
+	private String liquidityName;
+}
---
+++ asset-manager-app/asset-manager-webapp/src/main/java/com/asset/manager/service/impl/TreeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.asset.manager.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service(value = "treeService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class TreeServiceImpl
+{
+}
