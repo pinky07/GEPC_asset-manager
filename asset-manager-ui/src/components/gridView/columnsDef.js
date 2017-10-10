2dbc5d0e3c78d640437cbc52c7418e63284281bf
foza <francisco.zuniga@gft.com>
2017-10-10 10:33:14 -0600

    NEPCA-24-Grid View: Sort on Columns: added sort definition for columns, added icons, renamed files according to components

--- asset-manager-ui/src/components/gridView/columnsDef.js
+++ asset-manager-ui/src/components/gridView/columnsDef.js
@@ -6,41 +6,59 @@ export const columns = [
     headerName: 'Node Name',
     width: 350,
     cellRendererFramework: NodeNameCellRenderer,
+    sortingOrder: [null, null],
+    icons: {
+      sortUnSort: '<i class="fa fa-refresh fa-3" aria-hidden="true"/>',
+    },
   },
   {
     field: 'assetCategory',
     headerName: 'Asset Category',
     width: 184,
+    sortingOrder: ['asc','desc'],
+    icons: {
+      sortAscending: '<i class="fa fa-sort-asc fa-3" aria-hidden="true"/>',
+      sortDescending: '<i class="fa fa-sort-desc fa-3" aria-hidden="true"/>',
+    },
   },
   {
     field: 'accountgroupname',
     headerName: 'Asset Class Alias',
     width: 350,
+    sortingOrder: ['asc','desc'],
+    icons: {
+      sortAscending: '<i class="fa fa-sort-asc fa-3" aria-hidden="true"/>',
+      sortDescending: '<i class="fa fa-sort-desc fa-3" aria-hidden="true"/>',
+    },
   },
   {
     field: 'aa_model_benchmark',
     headerName: 'AAMB',
     width: 200,
+    sortingOrder: ['asc','desc'],
+    icons: {
+      sortAscending: '<i class="fa fa-sort-asc fa-3" aria-hidden="true"/>',
+      sortDescending: '<i class="fa fa-sort-desc fa-3" aria-hidden="true"/>',
+    },
   },
   {
     field: 'policy_value',
     headerName: 'Policy',
     width: 200,
+    sortingOrder: ['asc','desc'],
+    icons: {
+      sortAscending: '<i class="fa fa-sort-asc fa-3" aria-hidden="true"/>',
+      sortDescending: '<i class="fa fa-sort-desc fa-3" aria-hidden="true"/>',
+    },
   },
   {
     field: 'actual_mv',
     headerName: 'Value',
     width: 200,
-  },
-  {
-    field: 'mixA',
-    headerName: 'Mix A',
-    width: 200,
-    editable: true,
-  },
-  {
-    field: 'dummy',
-    headerName: '',
-    width: 200,
+    sortingOrder: ['asc','desc'],
+    icons: {
+      sortAscending: '<i class="fa fa-sort-asc fa-3" aria-hidden="true"/>',
+      sortDescending: '<i class="fa fa-sort-desc fa-3" aria-hidden="true"/>',
+    },
   },
 ];
---
+++ asset-manager-ui/src/tests/components/gridView/columnsDef.test.js
@@ -0,0 +1,144 @@
+import { columns } from '../../../components/gridView/columnsDef';
+
+describe('columns definition', () => {
+  it('should have the right number of columns', () => {
+    const columnCount = 6;
+    expect(columns.length).toEqual(columnCount);
+  });
+ 
+  describe('the first column', () => {
+    const firstColumn = columns[0];
+    it('should be the accountgroupname', () => {
+      const fieldName = 'accountgroupname';
+      expect(firstColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'Node Name';
+      expect(firstColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have no value for ascending sort', () => {
+      expect(firstColumn.sortingOrder[0]).toEqual(null);
+    });
+
+    it('should have no value for descending sort', () => {
+      expect(firstColumn.sortingOrder[1]).toEqual(null);
+    });
+  });
+
+  describe('the second column', () => {
+    const secondColumn = columns[1];
+    it('should be the assetCategory', () => {
+      const fieldName = 'assetCategory';
+      expect(secondColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'Asset Category';
+      expect(secondColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have right value for ascending sort', () => {
+      const sortOder = 'asc'
+      expect(secondColumn.sortingOrder[0]).toEqual(sortOder);
+    });
+
+    it('should have the right value for descending sort', () => {
+      const sortOder = 'desc'
+      expect(secondColumn.sortingOrder[1]).toEqual(sortOder);
+    });
+  });
+
+  describe('the third column', () => {
+    const thirdColumn = columns[2];
+    it('should be the accountgroupname', () => {
+      const fieldName = 'accountgroupname';
+      expect(thirdColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'Asset Class Alias';
+      expect(thirdColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have right value for ascending sort', () => {
+      const sortOder = 'asc'
+      expect(thirdColumn.sortingOrder[0]).toEqual(sortOder);
+    });
+
+    it('should have the right value for descending sort', () => {
+      const sortOder = 'desc'
+      expect(thirdColumn.sortingOrder[1]).toEqual(sortOder);
+    });
+  });
+
+  describe('the forth column', () => {
+    const forthColumn = columns[3];
+    it('should be the aa_model_benchmark', () => {
+      const fieldName = 'aa_model_benchmark';
+      expect(forthColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'AAMB';
+      expect(forthColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have right value for ascending sort', () => {
+      const sortOder = 'asc'
+      expect(forthColumn.sortingOrder[0]).toEqual(sortOder);
+    });
+
+    it('should have the right value for descending sort', () => {
+      const sortOder = 'desc'
+      expect(forthColumn.sortingOrder[1]).toEqual(sortOder);
+    });
+  });
+
+  describe('the fifth column', () => {
+    const fifthColumn = columns[4];
+    it('should be the policy_value', () => {
+      const fieldName = 'policy_value';
+      expect(fifthColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'Policy';
+      expect(fifthColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have right value for ascending sort', () => {
+      const sortOder = 'asc'
+      expect(fifthColumn.sortingOrder[0]).toEqual(sortOder);
+    });
+
+    it('should have the right value for descending sort', () => {
+      const sortOder = 'desc'
+      expect(fifthColumn.sortingOrder[1]).toEqual(sortOder);
+    });
+  });
+
+  describe('the sixth column', () => {
+    const sixthColumn = columns[5];
+    it('should be the actual_mv', () => {
+      const fieldName = 'actual_mv';
+      expect(sixthColumn.field).toEqual(fieldName);
+    });
+
+    it('should display the correct column name', () => {
+      const headerName = 'Value';
+      expect(sixthColumn.headerName).toEqual(headerName);
+    });
+
+    it('should have right value for ascending sort', () => {
+      const sortOder = 'asc'
+      expect(sixthColumn.sortingOrder[0]).toEqual(sortOder);
+    });
+
+    it('should have the right value for descending sort', () => {
+      const sortOder = 'desc'
+      expect(sixthColumn.sortingOrder[1]).toEqual(sortOder);
+    });
+  });
+});
